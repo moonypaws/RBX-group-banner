476456c2c3bc6d00c47fee50cf8405c59c98b23c
moonypaws <148655929+moonypaws@users.noreply.github.com>
2025-10-17 00:09:30 +0300

    Revise ban reasons and add comments in BannedGroupsModule
    Updated reasons for banning specific groups and added comments for clarity.

--- BannedGroupsModule.lua
+++ BannedGroupsModule.lua
@@ -1,40 +1,39 @@
 local forbiddenGroups = {
-	[7765801] = { -- faeki's group 1
-		IgnoreRoleLevel1 = true,
-		reason = "This group/community has a dark past or shady developers.",
-		banAscnAfterKick = true
-	},
-
-	[34804169] = { -- faeki's group 2
-		IgnoreRoleLevel1 = false,
-		reason = "Same reason as group ID [' 7765801 '].",
-		banAscnAfterKick = true
-	},
-
-	[33022518] = {-- faeki's group 3
-		IgnoreRoleLevel1 = false,
-		reason = "Same reason as group ID [' 7765801 '].",
-		banAscnAfterKick = true
-	},
-
-	[15630224] = {
-		IgnoreRoleLevel1 = false,
-		reason = "Suspicious owner.",
-		banAscnAfterKick = true
-	},
-
-	[15278613] = {
-		IgnoreRoleLevel1 = true,
-		reason = "Suspicious owner.",
-		banAscnAfterKick = true
-	},
-
-	[34999790] = { --test
+	--[[[34999790] = { -- testing reasons
 		IgnoreRoleLevel1 = true,
 		reason = "Testing.",
 		banAscnAfterKick = true
+	},]]
+	
+	[7765801] = { -- faeki's group 1 (mainly know for making the game: Transfur Outbreak)
+		IgnoreRoleLevel1 = true,
+		reason = "This group/community has a dark past or shady developers. Known for stealing assests from other games",
+		banAscnAfterKick = true
 	},
 
+	[34804169] = { -- faeki's group 2 (generally supporters)
+		IgnoreRoleLevel1 = false,
+		reason = "Same reason as group ID [' 7765801 '].",
+		banAscnAfterKick = true
+	},
+
+	[33022518] = {-- faeki's group 3 (generally supporters)
+		IgnoreRoleLevel1 = false,
+		reason = "Same reason as group ID [' 7765801 '].",
+		banAscnAfterKick = true
+	},
+
+	[15630224] = { -- group named "Sharkiiea's lounge"
+		IgnoreRoleLevel1 = false,
+		reason = "Suspicious owner.",
+		banAscnAfterKick = true
+	},
+
+	[15278613] = { -- group named "Unusual Assets"
+		IgnoreRoleLevel1 = true,
+		reason = "Suspicious owner.",
+		banAscnAfterKick = true
+	},
 }
 
 return forbiddenGroups
